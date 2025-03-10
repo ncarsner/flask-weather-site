ab90ba27e41bb10f81f3afb530f53ea9b1d3653d
Nicholas Carsner <nicholascarsner@gmail.com>
2025-03-10 15:45:38 -0500

    Added forecast function.

--- weather.py
+++ weather.py
@@ -16,6 +16,12 @@ def get_current_weather(city=random.choice(cities.world_cities)):
     return weather_data
 
 
+def get_five_day_forecast(city=random.choice(cities.world_cities)):
+    request_url = f'http://api.openweathermap.org/data/2.5/forecast?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+    forecast_data = requests.get(request_url).json()
+    return forecast_data
+
+
 if __name__ == "__main__":
     print("\n*** Get Current Weather Conditions ***")
 
@@ -25,8 +31,9 @@ if __name__ == "__main__":
     if not city.strip():
         city = random.choice(cities.world_cities)
 
-
     weather_data = get_current_weather(city)
+    forecast_data = get_five_day_forecast(city)
 
     print("\n")
     pprint(weather_data)
+    pprint(forecast_data)
